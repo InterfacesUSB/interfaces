fdc809c90459252a8253ef5e9a29b40490f44966
Stefani Castellanos <scct95@hotmail.com>
2016-11-23 21:19:06 -0400

    brillo de imagenes en iluminacion

--- jquery-js/iluminacion.js
+++ jquery-js/iluminacion.js
@@ -1,14 +1,9 @@
 $(document).ready(function () {
 
-    // $('#slider_kitchen').slider({
-    // 	formatter: function(value) {
-    // 		return 'Current value: ' + value;
-    // 	}
-    // });
-
     // kitchen slider
     var changeValue_kitchen = function() {
        $('#slider_kitchen_val').text(sliderKitchen.getValue());
+        changeBrightness('#slider_kitchen_val', '#img_kitchen');
     };
 
     var sliderKitchen = $('#slider_kitchen').slider()
@@ -18,6 +13,7 @@ $(document).ready(function () {
     // bathroom slider
     var changeValue_bathroom = function() {
        $('#slider_bathroom_val').text(sliderBathroom.getValue());
+       changeBrightness('#slider_bathroom_val', '#img_bathroom');
     };
 
     var sliderBathroom = $('#slider_bathroom').slider()
@@ -27,6 +23,7 @@ $(document).ready(function () {
     // room1 slider
     var changeValue_room1 = function() {
        $('#slider_room1_val').text(sliderRoom1.getValue());
+       changeBrightness('#slider_room1_val', '#img_room1');
     };
 
     var sliderRoom1 = $('#slider_room1').slider()
@@ -34,3 +31,31 @@ $(document).ready(function () {
     		.data('slider');
 
 });
+
+// function that changes the brightness of the image
+function changeBrightness(id_val, id_img) {
+    var val = $(id_val).text();
+
+    var brightness;
+    if (val === 0){
+        brightness = 17;
+    }
+    if (val < 11) {
+        brightness = 25;
+    }
+    else if (val > 10 && val < 30) {
+        brightness = 30;
+    }
+    else {
+        brightness = val;
+    }
+
+    var brightness_img = 'brightness(' + brightness + '%)';
+    $(id_img).css({
+           'filter' : brightness_img,
+           '-webkit-filter' : brightness_img,
+           '-moz-filter' : brightness_img,
+           '-o-filter' : brightness_img,
+           '-ms-filter' : brightness_img,
+    });
+}
