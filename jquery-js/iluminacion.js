efce8097bd98e465f6373940fc46327dae76f44b
Stefani Castellanos <scct95@hotmail.com>
2016-11-25 21:38:44 -0400

    iluminacion terminada (creo xD)

--- jquery-js/iluminacion.js
+++ jquery-js/iluminacion.js
@@ -1,6 +1,9 @@
 $(document).ready(function () {
 
     // living room slider
+
+    changeBrightness('#slider_living_room_val', '#img_living_room');
+
     var changeValue_living_room = function() {
        $('#slider_living_room_val').text(sliderLivingRoom.getValue());
         changeBrightness('#slider_living_room_val', '#img_living_room');
@@ -10,7 +13,20 @@ $(document).ready(function () {
     		.on('slide', changeValue_living_room)
     		.data('slider');
 
+    $('#electric_living_room').click(function () {
+        $('#electric_living_room').hide();
+        $('#solar_living_room').show();
+    });
+
+    $('#solar_living_room').click(function () {
+        $('#solar_living_room').hide();
+        $('#electric_living_room').show();
+    });
+
     // kitchen slider
+
+    changeBrightness('#slider_kitchen_val', '#img_kitchen');
+
     var changeValue_kitchen = function() {
        $('#slider_kitchen_val').text(sliderKitchen.getValue());
         changeBrightness('#slider_kitchen_val', '#img_kitchen');
@@ -20,7 +36,20 @@ $(document).ready(function () {
     		.on('slide', changeValue_kitchen)
     		.data('slider');
 
+    $('#electric_kitchen').click(function () {
+        $('#electric_kitchen').hide();
+        $('#solar_kitchen').show();
+    });
+
+    $('#solar_kitchen').click(function () {
+        $('#solar_kitchen').hide();
+        $('#electric_kitchen').show();
+    });
+
     // room1 slider
+
+    changeBrightness('#slider_room1_val', '#img_room1');
+
     var changeValue_room1 = function() {
        $('#slider_room1_val').text(sliderRoom1.getValue());
        changeBrightness('#slider_room1_val', '#img_room1');
@@ -30,7 +59,20 @@ $(document).ready(function () {
     		.on('slide', changeValue_room1)
     		.data('slider');
 
+    $('#electric_room1').click(function () {
+        $('#electric_room1').hide();
+        $('#solar_room1').show();
+    });
+
+    $('#solar_room1').click(function () {
+        $('#solar_room1').hide();
+        $('#electric_room1').show();
+    });
+
     // bathroom slider
+
+    changeBrightness('#slider_bathroom_val', '#img_bathroom');
+
     var changeValue_bathroom = function() {
        $('#slider_bathroom_val').text(sliderBathroom.getValue());
        changeBrightness('#slider_bathroom_val', '#img_bathroom');
@@ -39,6 +81,16 @@ $(document).ready(function () {
     var sliderBathroom = $('#slider_bathroom').slider()
     		.on('slide', changeValue_bathroom)
     		.data('slider');
+
+    $('#electric_bathroom').click(function () {
+        $('#electric_bathroom').hide();
+        $('#solar_bathroom').show();
+    });
+
+    $('#solar_bathroom').click(function () {
+        $('#solar_bathroom').hide();
+        $('#electric_bathroom').show();
+    });
 
 });
 
