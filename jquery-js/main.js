1e9044a014f7148cbfd612328e778b91b25f6d8d
Stefani Castellanos <scct95@hotmail.com>
2016-11-29 14:29:24 -0400

    notificaciones!!

--- jquery-js/main.js
+++ jquery-js/main.js
@@ -7,4 +7,17 @@ $( document ).ready(function() {
     $('.notification2').click(function () {
         $( "#notification2" ).fadeOut( "slow" );
     });
+
+    $('.notification3').click(function () {
+        $( "#notification3" ).fadeOut( "slow" );
+    });
+
+    $('.notification4').click(function () {
+        $( "#notification4" ).fadeOut( "slow" );
+    });
+
+    $('.notification5').click(function () {
+        $( "#notification5" ).fadeOut( "slow" );
+    });
+
 });
