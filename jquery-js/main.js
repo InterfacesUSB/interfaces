9afb1704d956b64706e1d4f1fcf06cb86f862147
Stefani Castellanos <scct95@hotmail.com>
2016-11-29 09:26:44 -0400

    mas arreglos en notificaciones/main

---
+++ jquery-js/main.js
@@ -0,0 +1,10 @@
+$( document ).ready(function() {
+
+    $('.notification1').click(function () {
+        $( "#notification1" ).fadeOut( "slow" );
+    });
+
+    $('.notification2').click(function () {
+        $( "#notification2" ).fadeOut( "slow" );
+    });
+});
